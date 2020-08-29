b146f512801f075dd910eab51d6473b55329ebf1
Anna <anna_agabekyan_anna@mail.ru>
2020-08-29 16:32:57 +0400

    feat: build sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <div className="app_body">
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
