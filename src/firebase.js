2d22adceaa0d2dfe525f0eab19ef26ec360e8de9
Anna <anna_agabekyan_anna@mail.ru>
2020-08-29 14:08:48 +0400

    feat: Build banner fb

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTU8RS6Zb2vaFH-aHrMK9SUPTPY1Ucclg",
+    authDomain: "facebook-41341.firebaseapp.com",
+    databaseURL: "https://facebook-41341.firebaseio.com",
+    projectId: "facebook-41341",
+    storageBucket: "facebook-41341.appspot.com",
+    messagingSenderId: "667716843271",
+    appId: "1:667716843271:web:9e993c63948b34777b77a2",
+    measurementId: "G-QC9GHFVGRG"
+  };
